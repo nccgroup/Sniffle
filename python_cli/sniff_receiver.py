f6ea581e707530d1b0c83e9efcb148d242fbfdd5
Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
2018-07-19 16:38:15 -0400

    Python script to parse sniffer output and print it
    Eventually I'll want to implement better dissection and storage,
    but this works for firmware testing purposes.

---
+++ python_cli/sniff_receiver.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import serial
+import sys, base64, struct
+
+def main(argv):
+    if len(argv) != 2:
+        print("Usage: sniff_receiver.py [serial_port_name]", file=sys.stderr)
+
+    ser = serial.Serial(argv[1], 460800)
+
+    while True:
+        pkt = ser.readline()
+        data = base64.b64decode(pkt.rstrip())
+        print_packet(data)
+
+def print_packet(data):
+    if data[0] != 0x10:
+        print("Not a BLE frame!", file=sys.stderr)
+        return
+
+    ts, l, rssi, chan = struct.unpack("<LBbB", data[1:8])
+    body = data[8:]
+
+    if len(body) != l:
+        print("Incorrect length field!", file=sys.stderr)
+        return
+
+    print("Timestamp: %.4f\tLength: %i\tRSSI: %i\tChannel: %i" % (
+        ts / 1000000., l, rssi, chan))
+    print(repr(data))
+    print()
+
+if __name__ == "__main__":
+    main(sys.argv)
