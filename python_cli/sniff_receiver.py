17e63ee4654a924bb003539ae7a143f117a38d55
Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
2018-07-26 11:20:56 -0400

    sniff_receiver: prettier hex printing

--- python_cli/sniff_receiver.py
+++ python_cli/sniff_receiver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import serial
-import sys, base64, struct
+import sys, binascii, base64, struct
 
 def main(argv):
     if len(argv) != 2:
@@ -11,28 +11,54 @@ def main(argv):
 
     while True:
         pkt = ser.readline()
-        data = base64.b64decode(pkt.rstrip())
-        print_packet(data)
+        try:
+            data = base64.b64decode(pkt.rstrip())
+        except binascii.Error as e:
+            print("Ignoring message:", e, file=sys.stderr)
+        print_message(data)
+
+def print_message(data):
+    if data[0] == 0x10:
+        print_packet(data[1:])
+    elif data[0] == 0x11: # debug print
+        print("DEBUG:", str(data[1:], encoding='utf-8'))
+        return
+    else:
+        print("Unknown message type!", file=sys.stderr)
+
+    print()
 
 def print_packet(data):
-    if data[0] == 0x11: # debug print
-        print(str(data[1:], encoding='utf-8'))
-        return
-    elif data[0] != 0x10:
-        print("Not a BLE frame!", file=sys.stderr)
-        return
-
-    ts, l, rssi, chan = struct.unpack("<LBbB", data[1:8])
-    body = data[8:]
+    ts, l, rssi, chan = struct.unpack("<LBbB", data[:7])
+    body = data[7:]
 
     if len(body) != l:
         print("Incorrect length field!", file=sys.stderr)
         return
 
-    print("Timestamp: %.4f\tLength: %i\tRSSI: %i\tChannel: %i" % (
+    print("Timestamp: %.6f\tLength: %i\tRSSI: %i\tChannel: %i" % (
         ts / 1000000., l, rssi, chan))
-    print(repr(body))
-    print()
+    if chan >= 37:
+        decode_advert(body)
+    else:
+        decode_data(body)
+
+def _safe_asciify(c):
+    if 32 <= c <= 126:
+        return chr(c)
+    return " "
+
+def print_hexdump(data):
+    ascstr = "  ".join([_safe_asciify(b) for b in data])
+    hexstr = " ".join(["%02X" % b for b in data])
+    print(ascstr)
+    print(hexstr)
+
+def decode_advert(body):
+    print_hexdump(body)
+
+def decode_data(body):
+    print_hexdump(body)
 
 if __name__ == "__main__":
     main(sys.argv)
