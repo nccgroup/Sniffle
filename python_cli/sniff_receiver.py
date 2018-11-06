1f0e15d8951f796aa062b91ce90d8a3688fd2809
Sultan Qasim Khan <Sultan.QasimKhan@nccgroup.trust>
2018-11-06 13:38:54 -0500

    Auto-activate advertisement hopping mode with MAC filtering

--- python_cli/sniff_receiver.py
+++ python_cli/sniff_receiver.py
@@ -51,6 +51,10 @@ def main():
     macMsg = base64.b64encode(macCmd) + b'\r\n'
     ser.write(macMsg)
 
+    if not (args.mac is None):
+        advHopMsg = base64.b64encode(bytes([0x01, 0x14])) + b'\r\n'
+        ser.write(advHopMsg)
+
     while True:
         pkt = ser.readline()
         try:
