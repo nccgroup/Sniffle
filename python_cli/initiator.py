35d757baec62ef2154db842f361a34812038337f
Sultan Qasim Khan <sultan.qasimkhan@nccgroup.com>
2020-03-12 21:44:34 -0400

    initiator: allow determining target MAC from IRK

--- python_cli/initiator.py
+++ python_cli/initiator.py
@@ -7,7 +7,8 @@
 import argparse, sys
 from pcap import PcapBleWriter
 from sniffle_hw import SniffleHW, BLE_ADV_AA, PacketMessage, DebugMessage, StateMessage, SnifferState
-from packet_decoder import DPacketMessage
+from packet_decoder import DPacketMessage, AdvaMessage, AdvDirectIndMessage, AdvExtIndMessage, str_mac
+from binascii import unhexlify
 
 # global variable to access hardware
 hw = None
@@ -21,6 +22,7 @@ def main():
     aparse.add_argument("-r", "--rssi", default=-80, type=int,
             help="Filter packets by minimum RSSI")
     aparse.add_argument("-m", "--mac", default=None, help="Specify target MAC address")
+    aparse.add_argument("-i", "--irk", default=None, help="Specify target IRK")
     aparse.add_argument("-l", "--longrange", action="store_const", default=False, const=True,
             help="Use long range (coded) PHY for primary advertising")
     args = aparse.parse_args()
@@ -28,8 +30,8 @@ def main():
     global hw
     hw = SniffleHW(args.serport)
 
-    if args.mac is None:
-        print("Must specify target MAC address", file=sys.stderr)
+    if args.mac is None and args.irk is None:
+        print("Must specify target MAC address or IRK", file=sys.stderr)
         return
 
     # set the advertising channel (and return to ad-sniffing mode)
@@ -44,20 +46,26 @@ def main():
     # configure RSSI filter
     hw.cmd_rssi(args.rssi)
 
-    try:
-        macBytes = [int(h, 16) for h in reversed(args.mac.split(":"))]
-        if len(macBytes) != 6:
-            raise Exception("Wrong length!")
-    except:
-        print("MAC must be 6 colon-separated hex bytes", file=sys.stderr)
-        return
-    hw.cmd_mac(macBytes, False)
+    if args.mac:
+        try:
+            macBytes = [int(h, 16) for h in reversed(args.mac.split(":"))]
+            if len(macBytes) != 6:
+                raise Exception("Wrong length!")
+        except:
+            print("MAC must be 6 colon-separated hex bytes", file=sys.stderr)
+            return
+        hw.cmd_mac(macBytes, False)
+    else:
+        hw.cmd_irk(unhexlify(args.irk), False)
 
     # initiator doesn't care about this setting, it always accepts aux
     hw.cmd_auxadv(False)
 
     # initiator needs a MAC address
     hw.random_addr()
+
+    if args.irk:
+        macBytes = get_mac_from_irk()
 
     # zero timestamps and flush old packets
     hw.mark_and_flush()
@@ -69,6 +77,21 @@ def main():
     while True:
         msg = hw.recv_and_decode()
         print_message(msg)
+
+# assumes sniffer is already configured to receive ads with IRK filter
+def get_mac_from_irk():
+    hw.mark_and_flush()
+    print("Waiting for advertisement with suitable RPA...")
+    while True:
+        msg = hw.recv_and_decode()
+        if not isinstance(msg, PacketMessage):
+            continue
+        dpkt = DPacketMessage.decode(msg)
+        if isinstance(dpkt, AdvaMessage) or \
+                isinstance(dpkt, AdvDirectIndMessage) or \
+                (isinstance(dpkt, AdvExtIndMessage) and dpkt.AdvA is not None):
+            print("Found target MAC: %s" % str_mac(dpkt.AdvA))
+            return dpkt.AdvA
 
 def print_message(msg):
     if isinstance(msg, PacketMessage):
