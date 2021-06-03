39dafaf36617866b5c6bf589f03217e963fffe2b
Sultan Qasim Khan <sultan.qasimkhan@nccgroup.com>
2021-06-03 19:58:05 -0400

    conf_queue: semi-reentrancy and using past events
    Not a perfect fix for concurrency, but since we added the command to
    manually add a channel map to the radio config queue, there are race
    conditions possible in enqueing, even if unlikely. This at least makes
    two radio config enqueing operations happening in different tasks
    reentrantly somewhat safe.
    
    Also, if there is a pending configuration update, this will ensure the
    channel map update that was manually added won't be lost by ensuring
    queue entries with instants in the past are still used. Instead, we now
    assume all entries in the queue are supposed to be applied in the order
    they are enqueued.

--- fw/conf_queue.c
+++ fw/conf_queue.c
@@ -5,64 +5,79 @@
  */
 
 #include <stddef.h>
+#include <stdint.h>
+#include <stdatomic.h>
 #include "conf_queue.h"
 
 #define MODULO_MASK 0x7
 
 static struct RadioConfig configs[MODULO_MASK + 1];
 static uint16_t nextInstants[MODULO_MASK + 1];
-static uint32_t qhead = 0; // add configs to this index
-static uint32_t qtail = 0; // remove configs from this index
+static volatile atomic_uint qhead; // add configs to this index
+static volatile atomic_uint qtail; // remove configs from this index
 
 static inline uint32_t rconf_qsize(void)
 {
-    return (qhead - qtail) & MODULO_MASK;
+    /* note: not thread safe, but I'm not going to bother adding
+     * a lock/mutex because:
+     * - things get added from interrupt contexts
+     * - a mutex would add overhead
+     * - risk of queue getting filled during a race is low
+     * - even if queue gets filled in a race, no memory safety risk,
+     *   just loss of queue entries
+     */
+    return atomic_load(&qhead) - atomic_load(&qtail);
 }
 
 void rconf_reset(void)
 {
-    qhead = 0;
-    qtail = 0;
+    atomic_store(&qhead, 0);
+    atomic_store(&qtail, 0);
 }
 
 void rconf_enqueue(uint16_t nextInstant, const struct RadioConfig *conf)
 {
-    if (rconf_qsize() == MODULO_MASK)
+    uint32_t qhead_;
+    uint32_t qsz = rconf_qsize();
+
+    if (qsz >= MODULO_MASK)
         return; // full
 
-    nextInstants[qhead] = nextInstant;
-    configs[qhead] = *conf;
-    qhead = (qhead + 1) & MODULO_MASK;
+    // atomic make queue insertion partially reentrancy safe
+    qhead_ = atomic_fetch_add(&qhead, 1) & MODULO_MASK;
+
+    nextInstants[qhead_] = nextInstant;
+    configs[qhead_] = *conf;
 }
 
 bool rconf_dequeue(uint16_t connEventCount, struct RadioConfig *conf)
 {
+    uint32_t qtail_;
+
     // nothing to do if empty
     if (!rconf_qsize())
         return false;
 
-    // discard the past
-    if (((nextInstants[qtail] - connEventCount) & 0xFFFF) >= 0x8000) {
-        qtail = (qtail + 1) & MODULO_MASK;
-        return false;
+    qtail_ = atomic_load(&qtail) & MODULO_MASK;
+
+    // dequeue current or past events
+    if (nextInstants[qtail_] == connEventCount ||
+            ((nextInstants[qtail_] - connEventCount) & 0xFFFF) >= 0x8000)
+    {
+        *conf = configs[qtail_];
+        atomic_fetch_add(&qtail, 1);
+        return true;
     }
 
     // wait on future events
-    if (connEventCount != nextInstants[qtail])
-        return false;
-
-    *conf = configs[qtail];
-    qtail = (qtail + 1) & MODULO_MASK;
-
-    return true;
+    return false;
 }
 
 const struct RadioConfig * rconf_latest(void)
 {
-    uint32_t last_head = (qhead - 1) & MODULO_MASK;
-
     if (!rconf_qsize())
         return NULL;
 
+    uint32_t last_head = (atomic_load(&qhead) - 1) & MODULO_MASK;
     return configs + last_head;
 }
