1f0e07a3f4fef390656836a3ca21c64c637dadf9
froloffw7 <69122526+froloffw7@users.noreply.github.com>
2024-04-11 15:13:44 +0200

    Create hexdump.py

---
+++ python_cli/hexdump.py
@@ -0,0 +1,29 @@
+# https://gist.github.com/NeatMonster/c06c61ba4114a2b31418a364341c26c0#file-hexdump-py
+class hexdump:
+    def __init__(self, buf, off=0):
+        self.buf = buf
+        self.off = off
+
+    def __iter__(self):
+        last_bs, last_line = None, None
+        for i in range(0, len(self.buf), 16):
+            bs = bytearray(self.buf[i : i + 16])
+            line = "{:08x}  {:23}  {:23}  |{:16}|".format(
+                self.off + i,
+                " ".join(("{:02x}".format(x) for x in bs[:8])),
+                " ".join(("{:02x}".format(x) for x in bs[8:])),
+                "".join((chr(x) if 32 <= x < 127 else "." for x in bs)),
+            )
+            if bs == last_bs:
+                line = "*"
+            if bs != last_bs or line != last_line:
+                yield line
+            last_bs, last_line = bs, line
+        yield "{:08x}".format(self.off + len(self.buf))
+
+    def __str__(self):
+        return "\n".join(self)
+
+    def __repr__(self):
+        return "\n".join(self)
+
