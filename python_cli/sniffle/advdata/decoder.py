c4bb402866821f14de71b2b3db36027d59d3243e
Sultan Qasim Khan <sultanqasim@gmail.com>
2024-06-04 12:42:32 -0400

    Flesh out advertising data decoding a bit
    Still much more to decode

--- python_cli/sniffle/advdata/decoder.py
+++ python_cli/sniffle/advdata/decoder.py
@@ -2,14 +2,90 @@
 # Copyright (c) 2024, NCC Group plc
 # Released as open source under GPLv3
 
+from struct import unpack
+
 class AdvDataRecord:
     def __init__(self, data_type: int, data: bytes):
         self.type = data_type
         self.data = data
 
+    def str_type(self):
+        return "AD Type: 0x%02X" % self.type
+
     def __str__(self):
-        return "AdvData Type: 0x%02X Length: %d Value: %s" % (
-                self.type, len(self.data), repr(self.data))
+        return "%s\n    Length: %d\n    Value: %s" % (
+                self.str_type(), len(self.data), repr(self.data))
+
+class FlagsRecord(AdvDataRecord):
+    def str_type(self):
+        return "Flags"
+
+    def __str__(self):
+        if len(self.data) == 1:
+            return "%s: 0x%02X" % (self.str_type(), self.data[0])
+        else:
+            return "%s: Malformed" % self.str_type()
+
+class LocalNameRecord(AdvDataRecord):
+    def __str__(self):
+        try:
+            name = str(self.data, encoding='utf-8')
+        except:
+            name = repr(self.data) + " (Invalid UTF-8)"
+        return "%s: %s" % (self.str_type(), name)
+
+class ShortenedLocalNameRecord(LocalNameRecord):
+    def str_type(self):
+        return "Shortened Local Name"
+
+class CompleteLocalNameRecord(LocalNameRecord):
+    def str_type(self):
+        return "Complete Local Name"
+
+class TXPowerLevelRecord(AdvDataRecord):
+    def str_type(self):
+        return "TX Power Level"
+
+    def __str__(self):
+        if len(self.data) == 1:
+            power, = unpack('<b', self.data)
+            return "%s: %d dBm" % (self.str_type(), power)
+        else:
+            return "%s: Malformed" % self.str_type()
+
+class ServiceData16Record(AdvDataRecord):
+    def str_type(self):
+        return "Service Data - 16-bit UUID"
+
+class ServiceData32Record(AdvDataRecord):
+    def str_type(self):
+        return "Service Data - 32-bit UUID"
+
+class ServiceData128Record(AdvDataRecord):
+    def str_type(self):
+        return "Service Data - 128-bit UUID"
+
+class ManufacturerSpecificDataRecord(AdvDataRecord):
+    def str_type(self):
+        return "Manufacturer Specific Data"
+
+# https://bitbucket.org/bluetooth-SIG/public/src/main/assigned_numbers/core/ad_types.yaml
+ad_types = {
+    0x01: FlagsRecord,
+    0x08: ShortenedLocalNameRecord,
+    0x09: CompleteLocalNameRecord,
+    0x0A: TXPowerLevelRecord,
+    0x16: ServiceData16Record,
+    0x20: ServiceData32Record,
+    0x21: ServiceData128Record,
+    0xFF: ManufacturerSpecificDataRecord
+}
+
+def record_from_type_data(data_type: int, data: bytes):
+    if data_type in ad_types:
+        return ad_types[data_type](data_type, data)
+    else:
+        return AdvDataRecord(data_type, data)
 
 def decode_adv_data(data):
     records = []
@@ -20,9 +96,9 @@ def decode_adv_data(data):
             l = data[i]
             t = data[i+1]
             d = data[i+2:i+1+l]
+            records.append(record_from_type_data(t, d))
+            i += 1+l
         except:
             break
-        records.append(AdvDataRecord(t, d))
-        i += 1+l
 
     return records
