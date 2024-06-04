44a6ff1a24a8562206772ce4456892cdbde6789d
Sultan Qasim Khan <sultanqasim@gmail.com>
2024-06-04 16:37:03 -0400

    advdata: take AD type name from assigned numbers

--- python_cli/sniffle/advdata/decoder.py
+++ python_cli/sniffle/advdata/decoder.py
@@ -3,6 +3,7 @@
 # Released as open source under GPLv3
 
 from struct import unpack
+from .constants import ad_types
 
 class AdvDataRecord:
     def __init__(self, data_type: int, data: bytes):
@@ -10,16 +11,16 @@ class AdvDataRecord:
         self.data = data
 
     def str_type(self):
-        return "AD Type: 0x%02X" % self.type
+        if self.type in ad_types:
+            return ad_types[self.type]
+        else:
+            return "Unknown Advertising Data Type: 0x%02X" % self.type
 
     def __str__(self):
         return "%s\n    Length: %d\n    Value: %s" % (
                 self.str_type(), len(self.data), repr(self.data))
 
 class FlagsRecord(AdvDataRecord):
-    def str_type(self):
-        return "Flags"
-
     def __str__(self):
         if len(self.data) == 1:
             return "%s: 0x%02X" % (self.str_type(), self.data[0])
@@ -27,16 +28,13 @@ class FlagsRecord(AdvDataRecord):
             return "%s: Malformed" % self.str_type()
 
 class ServiceList16Record(AdvDataRecord):
-    def str_type(self):
-        return "Complete List of 16-bit Service Class UUIDs"
+    pass
 
 class ServiceList32Record(AdvDataRecord):
-    def str_type(self):
-        return "Complete List of 32-bit Service Class UUIDs"
+    pass
 
 class ServiceList128Record(AdvDataRecord):
-    def str_type(self):
-        return "Complete List of 128-bit Service Class UUIDs"
+    pass
 
 class LocalNameRecord(AdvDataRecord):
     def __str__(self):
@@ -47,17 +45,12 @@ class LocalNameRecord(AdvDataRecord):
         return "%s: %s" % (self.str_type(), name)
 
 class ShortenedLocalNameRecord(LocalNameRecord):
-    def str_type(self):
-        return "Shortened Local Name"
+    pass
 
 class CompleteLocalNameRecord(LocalNameRecord):
-    def str_type(self):
-        return "Complete Local Name"
+    pass
 
 class TXPowerLevelRecord(AdvDataRecord):
-    def str_type(self):
-        return "TX Power Level"
-
     def __str__(self):
         if len(self.data) == 1:
             power, = unpack('<b', self.data)
@@ -66,23 +59,19 @@ class TXPowerLevelRecord(AdvDataRecord):
             return "%s: Malformed" % self.str_type()
 
 class ServiceData16Record(AdvDataRecord):
-    def str_type(self):
-        return "Service Data - 16-bit UUID"
+    pass
 
 class ServiceData32Record(AdvDataRecord):
-    def str_type(self):
-        return "Service Data - 32-bit UUID"
+    pass
 
 class ServiceData128Record(AdvDataRecord):
-    def str_type(self):
-        return "Service Data - 128-bit UUID"
+    pass
 
 class ManufacturerSpecificDataRecord(AdvDataRecord):
-    def str_type(self):
-        return "Manufacturer Specific Data"
+    pass
 
 # https://bitbucket.org/bluetooth-SIG/public/src/main/assigned_numbers/core/ad_types.yaml
-ad_types = {
+ad_type_classes = {
     0x01: FlagsRecord,
     0x03: ServiceList16Record,
     0x05: ServiceList32Record,
@@ -97,8 +86,8 @@ ad_types = {
 }
 
 def record_from_type_data(data_type: int, data: bytes):
-    if data_type in ad_types:
-        return ad_types[data_type](data_type, data)
+    if data_type in ad_type_classes:
+        return ad_type_classes[data_type](data_type, data)
     else:
         return AdvDataRecord(data_type, data)
 
