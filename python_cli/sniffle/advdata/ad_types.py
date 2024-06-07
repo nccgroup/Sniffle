a3673c1667711db45e7b9c330748c65073edaa69
Sultan Qasim Khan <sultan.qasimkhan@nccgroup.com>
2024-06-07 17:15:31 -0400

    advdata: automate AD printing indentation

--- python_cli/sniffle/advdata/ad_types.py
+++ python_cli/sniffle/advdata/ad_types.py
@@ -36,63 +36,69 @@ class AdvDataRecord:
         else:
             return "Unknown Advertising Data Type: 0x%02X" % self.type
 
+    def str_lines(self):
+        lines = []
+        lines.append(self.str_type())
+        lines.append("Length: %d" % len(self.data))
+        lines.append("Value: %s" % repr(self.data))
+        return lines
+
     def __str__(self):
-        return "%s\n    Length: %d\n    Value: %s" % (
-                self.str_type(), len(self.data), repr(self.data))
+        return "\n    ".join(self.str_lines())
 
 class FlagsRecord(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         if len(self.data) == 1:
-            return "%s: 0x%02X" % (self.str_type(), self.data[0])
+            return ["%s: 0x%02X" % (self.str_type(), self.data[0])]
         else:
-            return "%s: Malformed" % self.str_type()
+            return ["%s: Malformed" % self.str_type()]
 
 class ServiceList16Record(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         lines = [self.str_type()]
         if len(self.data) % 2 == 0:
             for i in range(0, len(self.data), 2):
                 u, = unpack('<H', self.data[i:i+2])
-                lines.append("    %s" % str_service16(u))
+                lines.append(str_service16(u))
         else:
-            lines.append("    Malformed")
-            lines.append("    Length: %d" % len(self.data))
-            lines.append("    Value: %s" % repr(self.data))
-        return "\n".join(lines)
+            lines.append("Malformed")
+            lines.append("Length: %d" % len(self.data))
+            lines.append("Value: %s" % repr(self.data))
+        return lines
 
 class ServiceList32Record(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         lines = [self.str_type()]
         if len(self.data) % 4 == 0:
             for i in range(0, len(self.data), 4):
                 u, = unpack('<I', self.data[i:i+4])
-                lines.append("    %s" % str_service32(u))
+                lines.append(str_service32(u))
         else:
-            lines.append("    Malformed")
-            lines.append("    Length: %d" % len(self.data))
-            lines.append("    Value: %s" % repr(self.data))
-        return "\n".join(lines)
+            lines.append("Malformed")
+            lines.append("Length: %d" % len(self.data))
+            lines.append("Value: %s" % repr(self.data))
+        return lines
 
 class ServiceList128Record(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         lines = [self.str_type()]
         if len(self.data) % 16 == 0:
             for i in range(0, len(self.data), 16):
                 u = self.data[i:i+16]
-                lines.append("    %s" % str_service128(u))
+                lines.append(str_service128(u))
         else:
-            lines.append("    Malformed")
-            lines.append("    Length: %d" % len(self.data))
-            lines.append("    Value: %s" % repr(self.data))
-        return "\n".join(lines)
+            lines.append("Malformed")
+            lines.append("Length: %d" % len(self.data))
+            lines.append("Value: %s" % repr(self.data))
+        return lines
 
 class LocalNameRecord(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         try:
             name = str(self.data, encoding='utf-8')
         except:
             name = repr(self.data) + " (Invalid UTF-8)"
-        return "%s: %s" % (self.str_type(), name)
+        return ["%s: %s" % (self.str_type(), name)]
 
 class ShortenedLocalNameRecord(LocalNameRecord):
     pass
@@ -101,69 +107,69 @@ class CompleteLocalNameRecord(LocalNameRecord):
     pass
 
 class TXPowerLevelRecord(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         if len(self.data) == 1:
             power, = unpack('<b', self.data)
-            return "%s: %d dBm" % (self.str_type(), power)
+            return ["%s: %d dBm" % (self.str_type(), power)]
         else:
-            return "%s: Malformed" % self.str_type()
+            return ["%s: Malformed" % self.str_type()]
 
 class ServiceData16Record(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         lines = [self.str_type()]
         if len(self.data) >= 2:
             u, = unpack('<H', self.data[:2])
             d = self.data[2:]
-            lines.append("    Service: %s" % str_service16(u))
-            lines.append("    Data Length: %d" % len(d))
-            lines.append("    Data: %s" % repr(d))
+            lines.append("Service: %s" % str_service16(u))
+            lines.append("Data Length: %d" % len(d))
+            lines.append("Data: %s" % repr(d))
         else:
-            lines.append("    Malformed")
-            lines.append("    Length: %d" % len(self.data))
-            lines.append("    Value: %s" % repr(self.data))
-        return "\n".join(lines)
+            lines.append("Malformed")
+            lines.append("Length: %d" % len(self.data))
+            lines.append("Value: %s" % repr(self.data))
+        return lines
 
 class ServiceData32Record(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         lines = [self.str_type()]
         if len(self.data) >= 4:
             u, = unpack('<I', self.data[:4])
             d = self.data[4:]
-            lines.append("    Service: %s" % str_service32(u))
-            lines.append("    Data Length: %d" % len(d))
-            lines.append("    Data: %s" % repr(d))
+            lines.append("Service: %s" % str_service32(u))
+            lines.append("Data Length: %d" % len(d))
+            lines.append("Data: %s" % repr(d))
         else:
-            lines.append("    Malformed")
-            lines.append("    Length: %d" % len(self.data))
-            lines.append("    Value: %s" % repr(self.data))
-        return "\n".join(lines)
+            lines.append("Malformed")
+            lines.append("Length: %d" % len(self.data))
+            lines.append("Value: %s" % repr(self.data))
+        return lines
 
 class ServiceData128Record(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         lines = [self.str_type()]
         if len(self.data) >= 16:
             u = self.data[:16]
             d = self.data[16:]
-            lines.append("    Service: %s" % str_service128(u))
-            lines.append("    Data Length: %d" % len(d))
-            lines.append("    Data: %s" % repr(d))
+            lines.append("Service: %s" % str_service128(u))
+            lines.append("Data Length: %d" % len(d))
+            lines.append("Data: %s" % repr(d))
         else:
-            lines.append("    Malformed")
-            lines.append("    Length: %d" % len(self.data))
-            lines.append("    Value: %s" % repr(self.data))
-        return "\n".join(lines)
+            lines.append("Malformed")
+            lines.append("Length: %d" % len(self.data))
+            lines.append("Value: %s" % repr(self.data))
+        return lines
 
 class ManufacturerSpecificDataRecord(AdvDataRecord):
-    def __str__(self):
+    def str_lines(self):
         lines = [self.str_type()]
         if len(self.data) >= 2:
             m, = unpack('<H', self.data[:2])
             d = self.data[2:]
-            lines.append("    Company: %s" % str_mfg(m))
-            lines.append("    Data Length: %d" % len(d))
-            lines.append("    Data: %s" % repr(d))
+            lines.append("Company: %s" % str_mfg(m))
+            lines.append("Data Length: %d" % len(d))
+            lines.append("Data: %s" % repr(d))
         else:
-            lines.append("    Malformed")
-            lines.append("    Length: %d" % len(self.data))
-            lines.append("    Value: %s" % repr(self.data))
-        return "\n".join(lines)
+            lines.append("Malformed")
+            lines.append("Length: %d" % len(self.data))
+            lines.append("Value: %s" % repr(self.data))
+        return lines
