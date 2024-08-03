043dc71c7a63d6d4452dcaa2420d5d598d07026b
Sultan Qasim Khan <sultan.qasimkhan@nccgroup.com>
2024-08-02 21:19:56 -0400

    channelizer: avoid unnecessary initialization

--- python_cli/sniffle/channelizer.py
+++ python_cli/sniffle/channelizer.py
@@ -42,7 +42,7 @@ class PolyphaseChannelizer:
         output_len = (len(samples) - len(self.filter_ic)) // self.channel_count
         self.filter_ic = samples[-len(self.filter_ic):]
 
-        filtered_samps = numpy.zeros((self.channel_count, output_len + 1), dtype=samples.dtype)
+        filtered_samps = numpy.empty((self.channel_count, output_len + 1), dtype=samples.dtype)
         filtered_samps[1:, 0] = self.extra
         self.extra = filtered_samps[1:, -1]
 
